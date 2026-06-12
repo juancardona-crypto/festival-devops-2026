cc223eaa6842da2212aad335e60b39a0158b3bd7
Juan Diego Cardona <juancardona1611@gmail.com>
2026-06-12 07:43:55 -0500

    Se agregan precios vip y general

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,10 @@ def home():
 @app.route('/artistas')
 def artistas():
     return jsonify({'artistas': ['Imagine Dragons', 'The Warning', 'Daft Punk', 'Maneskin']})
+    
+@app.route('/prices')
+def prices():
+    return jsonify({'prices': {'VIP': 200, 'General': 100}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
