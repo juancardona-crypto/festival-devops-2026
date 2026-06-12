743f31e3ca328e9d84f8432106c47e0473b44cb2
Juan Diego Cardona <juancardona1611@gmail.com>
2026-06-12 07:45:46 -0500

    Se agrega contacto

--- backend/app.py
+++ backend/app.py
@@ -14,5 +14,9 @@ def artistas():
 def prices():
     return jsonify({'prices': {'VIP': 200, 'General': 100}})
 
+@app.route('/contacto')
+def contacto():
+    return jsonify({'contacto': {'email': 'info@festivaldevops.com', 'telefono': '300 123 4567'}})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
